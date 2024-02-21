47080e8c46791006a8767f86a6f555007d6ef8a9
ikk1 <deus_junior1@yahoo.com.br>
2024-02-21 20:44:27 -0300

    first commit

---
+++ src/main/java/com/junior/studentRegistrationService/StudentRegistrationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.junior.studentRegistrationService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentRegistrationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentRegistrationServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/junior/studentRegistrationService/StudentRegistrationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.junior.studentRegistrationService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentRegistrationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
