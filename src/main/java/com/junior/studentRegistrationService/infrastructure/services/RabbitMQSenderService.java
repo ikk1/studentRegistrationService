024d7170dd818eb3d5fffbbb4a3e8f8b4c9e7619
ikk1 <deus_junior1@yahoo.com.br>
2024-02-23 18:46:04 -0300

    removed gson

--- src/main/java/com/junior/studentRegistrationService/infrastructure/services/RabbitMQSenderService.java
+++ src/main/java/com/junior/studentRegistrationService/infrastructure/services/RabbitMQSenderService.java
@@ -3,21 +3,24 @@ package com.junior.studentRegistrationService.infrastructure.services;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.stereotype.Service;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.junior.studentRegistrationService.infrastructure.messaging.StudentCreatedMessage;
 
 @Service
 public class RabbitMQSenderService {
 
     private RabbitTemplate rabbitTemplate;
+    private ObjectMapper parser;
 
-    public RabbitMQSenderService(RabbitTemplate rabbitTemplate) {
+    public RabbitMQSenderService(RabbitTemplate rabbitTemplate, ObjectMapper parser) {
         this.rabbitTemplate = rabbitTemplate;
+        this.parser = parser;
     }
 
     public void sendMessage(String name, String email) {
         try {
             StudentCreatedMessage message = new StudentCreatedMessage(name, email);
-            rabbitTemplate.convertAndSend("student.exchange", "student.created", message);
+            rabbitTemplate.convertAndSend("student.exchange", "student.created", parser.writeValueAsString(message));
         } catch (Exception e) {
             e.printStackTrace();
         }
